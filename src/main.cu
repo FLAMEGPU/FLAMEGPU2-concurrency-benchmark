9cac7e9213eb4efef22f84bf2ccfe5ee677b2e73
MILeach <matt-leach@hotmail.co.uk>
2021-03-10 14:59:08 +0000

    Brute force implementation

--- src/main.cu
+++ src/main.cu
@@ -129,6 +129,20 @@ FLAMEGPU_AGENT_FUNCTION(outputdata, MsgNone, MsgSpatial3D) {
     return ALIVE;
 }
 )###";
+
+const char* outputdataBruteForce = R"###(
+    FLAMEGPU_AGENT_FUNCTION(outputdata, MsgNone, MsgBruteForce) {
+        // Output each agents publicly visible properties.
+        FLAMEGPU->message_out.setVariable<int>("id", FLAMEGPU->getVariable<int>("id"));
+        FLAMEGPU->message_out.setVariable<float>("x", FLAMEGPU->getVariable<float>("x"));
+        FLAMEGPU->message_out.setVariable<float>("y", FLAMEGPU->getVariable<float>("y"));
+        FLAMEGPU->message_out.setVariable<float>("z", FLAMEGPU->getVariable<float>("z"));
+        FLAMEGPU->message_out.setVariable<float>("fx", FLAMEGPU->getVariable<float>("fx"));
+        FLAMEGPU->message_out.setVariable<float>("fy", FLAMEGPU->getVariable<float>("fy"));
+        FLAMEGPU->message_out.setVariable<float>("fz", FLAMEGPU->getVariable<float>("fz"));
+        return ALIVE;
+    }
+    )###";
 /**
  * inputdata agent function for Boid agents, which reads data from neighbouring Boid agents, to perform the boid flocking model.
  */
@@ -331,13 +345,213 @@ FLAMEGPU_AGENT_FUNCTION(inputdata, MsgSpatial3D, MsgNone) {
 }
 )###";
 
+const char* inputdataBruteForce = R"###(
+    // Vector utility functions, see top of file for versions with commentary
+    FLAMEGPU_HOST_DEVICE_FUNCTION float vec3Length(const float x, const float y, const float z) {
+        return sqrtf(x * x + y * y + z * z);
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void vec3Add(float &x, float &y, float &z, const float value) {
+        x += value;
+        y += value;
+        z += value;
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void vec3Sub(float &x, float &y, float &z, const float value) {
+        x -= value;
+        y -= value;
+        z -= value;
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void vec3Mult(float &x, float &y, float &z, const float multiplier) {
+        x *= multiplier;
+        y *= multiplier;
+        z *= multiplier;
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void vec3Div(float &x, float &y, float &z, const float divisor) {
+        x /= divisor;
+        y /= divisor;
+        z /= divisor;
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void vec3Normalize(float &x, float &y, float &z) {
+        // Get the length
+        float length = vec3Length(x, y, z);
+        vec3Div(x, y, z, length);
+    }
+    FLAMEGPU_HOST_DEVICE_FUNCTION void clampPosition(float &x, float &y, float &z, const float MIN_POSITION, const float MAX_POSITION) {
+        x = (x < MIN_POSITION)? MIN_POSITION: x;
+        x = (x > MAX_POSITION)? MAX_POSITION: x;
+    
+        y = (y < MIN_POSITION)? MIN_POSITION: y;
+        y = (y > MAX_POSITION)? MAX_POSITION: y;
+    
+        z = (z < MIN_POSITION)? MIN_POSITION: z;
+        z = (z > MAX_POSITION)? MAX_POSITION: z;
+    }
+    // Agent function
+    FLAMEGPU_AGENT_FUNCTION(inputdata, MsgBruteForce, MsgNone) {
+    
+        // Agent properties in local register
+        int id = FLAMEGPU->getVariable<int>("id");
+        // Agent position
+        float agent_x = FLAMEGPU->getVariable<float>("x");
+        float agent_y = FLAMEGPU->getVariable<float>("y");
+        float agent_z = FLAMEGPU->getVariable<float>("z");
+        // Agent velocity
+        float agent_fx = FLAMEGPU->getVariable<float>("fx");
+        float agent_fy = FLAMEGPU->getVariable<float>("fy");
+        float agent_fz = FLAMEGPU->getVariable<float>("fz");
+    
+        // Boids percieved center
+        float perceived_centre_x = 0.0f;
+        float perceived_centre_y = 0.0f;
+        float perceived_centre_z = 0.0f;
+        int perceived_count = 0;
+    
+        // Boids global velocity matching
+        float global_velocity_x = 0.0f;
+        float global_velocity_y = 0.0f;
+        float global_velocity_z = 0.0f;
+    
+        // Boids short range avoidance centre
+        float collision_centre_x = 0.0f;
+        float collision_centre_y = 0.0f;
+        float collision_centre_z = 0.0f;
+        int collision_count = 0;
+    
+        const float INTERACTION_RADIUS = FLAMEGPU->environment.getProperty<float>("INTERACTION_RADIUS");
+        const float SEPARATION_RADIUS = FLAMEGPU->environment.getProperty<float>("SEPARATION_RADIUS");
+        // Iterate location messages, accumulating relevant data and counts.
+        for (const auto &message : FLAMEGPU->message_in) {
+            // Ignore self messages.
+            if (message.getVariable<int>("id") != id) {
+                // Get the message location and velocity.
+                const float message_x = message.getVariable<float>("x");
+                const float message_y = message.getVariable<float>("y");
+                const float message_z = message.getVariable<float>("z");
+                const float message_fx = message.getVariable<float>("fx");
+                const float message_fy = message.getVariable<float>("fy");
+                const float message_fz = message.getVariable<float>("fz");
+    
+                // Check interaction radius
+                float separation = vec3Length(agent_x - message_x, agent_y - message_y, agent_z - message_z);
+    
+                if (separation < (INTERACTION_RADIUS)) {
+                    // Update the percieved centre
+                    perceived_centre_x += message_x;
+                    perceived_centre_y += message_y;
+                    perceived_centre_z += message_z;
+                    perceived_count++;
+    
+                    // Update percieved velocity matching
+                    global_velocity_x += message_fx;
+                    global_velocity_y += message_fy;
+                    global_velocity_z += message_fz;
+    
+                    // Update collision centre
+                    if (separation < (SEPARATION_RADIUS)) {  // dependant on model size
+                        collision_centre_x += message_x;
+                        collision_centre_y += message_y;
+                        collision_centre_z += message_z;
+                        collision_count += 1;
+                    }
+                }
+            }
+        }
+    
+        // Divide positions/velocities by relevant counts.
+        vec3Div(perceived_centre_x, perceived_centre_y, perceived_centre_z, perceived_count);
+        vec3Div(global_velocity_x, global_velocity_y, global_velocity_z, perceived_count);
+        vec3Div(global_velocity_x, global_velocity_y, global_velocity_z, collision_count);
+    
+        // Total change in velocity
+        float velocity_change_x = 0.f;
+        float velocity_change_y = 0.f;
+        float velocity_change_z = 0.f;
+    
+        // Rule 1) Steer towards perceived centre of flock (Cohesion)
+        float steer_velocity_x = 0.f;
+        float steer_velocity_y = 0.f;
+        float steer_velocity_z = 0.f;
+        if (perceived_count > 0) {
+            const float STEER_SCALE = FLAMEGPU->environment.getProperty<float>("STEER_SCALE");
+            steer_velocity_x = (perceived_centre_x - agent_x) * STEER_SCALE;
+            steer_velocity_y = (perceived_centre_y - agent_y) * STEER_SCALE;
+            steer_velocity_z = (perceived_centre_z - agent_z) * STEER_SCALE;
+        }
+        velocity_change_x += steer_velocity_x;
+        velocity_change_y += steer_velocity_y;
+        velocity_change_z += steer_velocity_z;
+    
+        // Rule 2) Match neighbours speeds (Alignment)
+        float match_velocity_x = 0.f;
+        float match_velocity_y = 0.f;
+        float match_velocity_z = 0.f;
+        if (collision_count > 0) {
+            const float MATCH_SCALE = FLAMEGPU->environment.getProperty<float>("MATCH_SCALE");
+            match_velocity_x = global_velocity_x * MATCH_SCALE;
+            match_velocity_y = global_velocity_y * MATCH_SCALE;
+            match_velocity_z = global_velocity_z * MATCH_SCALE;
+        }
+        velocity_change_x += match_velocity_x;
+        velocity_change_y += match_velocity_y;
+        velocity_change_z += match_velocity_z;
+    
+        // Rule 3) Avoid close range neighbours (Separation)
+        float avoid_velocity_x = 0.0f;
+        float avoid_velocity_y = 0.0f;
+        float avoid_velocity_z = 0.0f;
+        if (collision_count > 0) {
+            const float COLLISION_SCALE = FLAMEGPU->environment.getProperty<float>("COLLISION_SCALE");
+            avoid_velocity_x = (agent_x - collision_centre_x) * COLLISION_SCALE;
+            avoid_velocity_y = (agent_y - collision_centre_y) * COLLISION_SCALE;
+            avoid_velocity_z = (agent_z - collision_centre_z) * COLLISION_SCALE;
+        }
+        velocity_change_x += avoid_velocity_x;
+        velocity_change_y += avoid_velocity_y;
+        velocity_change_z += avoid_velocity_z;
+    
+        // Global scale of velocity change
+        vec3Mult(velocity_change_x, velocity_change_y, velocity_change_z, FLAMEGPU->environment.getProperty<float>("GLOBAL_SCALE"));
+    
+        // Update agent velocity
+        agent_fx += velocity_change_x;
+        agent_fy += velocity_change_y;
+        agent_fz += velocity_change_z;
+    
+        // Bound velocity
+        float agent_fscale = vec3Length(agent_fx, agent_fy, agent_fz);
+        if (agent_fscale > 1) {
+            vec3Div(agent_fx, agent_fy, agent_fz, agent_fscale);
+        }
+    
+        // Apply the velocity
+        const float TIME_SCALE = FLAMEGPU->environment.getProperty<float>("TIME_SCALE");
+        agent_x += agent_fx * TIME_SCALE;
+        agent_y += agent_fy * TIME_SCALE;
+        agent_z += agent_fz * TIME_SCALE;
+    
+        // Bound position
+        clampPosition(agent_x, agent_y, agent_z, FLAMEGPU->environment.getProperty<float>("MIN_POSITION"), FLAMEGPU->environment.getProperty<float>("MAX_POSITION"));
+    
+        // Update global agent memory.
+        FLAMEGPU->setVariable<float>("x", agent_x);
+        FLAMEGPU->setVariable<float>("y", agent_y);
+        FLAMEGPU->setVariable<float>("z", agent_z);
+    
+        FLAMEGPU->setVariable<float>("fx", agent_fx);
+        FLAMEGPU->setVariable<float>("fy", agent_fy);
+        FLAMEGPU->setVariable<float>("fz", agent_fz);
+    
+        return ALIVE;
+    }
+    )###";
+
 typedef struct Experiment { 
 
     Experiment(std::string title, 
         unsigned int initialPopSize, unsigned int finalPopSize, unsigned int popSizeIncrement, 
         unsigned int initialNumSpecies, unsigned int finalNumSpecies, unsigned int numSpeciesIncrement,
         unsigned int repetitions,
-        double targetPopDensity) {
+        double targetPopDensity,
+        bool spatial) {
             this->title = title;
             this->initialPopSize = initialPopSize;
             this->finalPopSize = finalPopSize;
@@ -347,6 +561,7 @@ typedef struct Experiment {
             this->numSpeciesIncrement = numSpeciesIncrement;
             this->repetitions = repetitions;
             this->targetPopDensity = targetPopDensity;
+            this->spatial = spatial;
         }
     std::string title;
     unsigned int initialPopSize = 100000;
@@ -359,17 +574,21 @@ typedef struct Experiment {
 
     unsigned int repetitions = 1;
     double targetPopDensity = 1024;
+    bool spatial = true;
 } Experiment;
 
 int main(int argc, const char ** argv) {
 
-    Experiment smallFixedPop("Small Fixed Pop", 512, 512, 512, 1, 16, 1, 5, 1024);
-    Experiment smallPops("Small Pops", 128, 1024, 128, 1, 8, 1, 5, 1024);
-    Experiment largePops("Large Pops", 1024, 8192, 1024, 1, 8, 1, 5, 1024);
-    Experiment deviceMaxed("Device Maxed", 100000, 100000, 100000, 1, 8, 1, 1, 4096);
+    Experiment smallFixedPop("Small Fixed Pop", 512, 512, 512, 1, 16, 1, 5, 1024, true);
+    Experiment smallPops("Small Pops", 128, 1024, 128, 1, 8, 1, 5, 1024, true);
+    Experiment largePops("Large Pops", 1024, 8192, 1024, 1, 8, 1, 5, 1024, true);
+    Experiment deviceMaxed("Device Maxed", 100000, 100000, 100000, 1, 8, 1, 1, 4096, true);
     //Experiment sweepPopDensity("Sweep Population Density", 4096, 4096, 4096, 1, 8, 1);
-    
-    std::vector<Experiment> experiments = {deviceMaxed};
+
+    Experiment largePopsBruteForce("Large Pops Brute Force", 1024, 1024, 1024, 1, 8, 1, 1, 1024, false);
+    Experiment largePopsSpatial("Large Pops Spatial", 1024, 1024, 1024, 1, 8, 1, 1, 1024, true);
+
+    std::vector<Experiment> experiments = {largePopsBruteForce};
     
     for (Experiment experiment : experiments) {
         
@@ -445,20 +664,33 @@ int main(int argc, const char ** argv) {
                             for (unsigned int i = 0; i < populationSizes.size(); i++) {
                                 std::string messageName = "location";
                                 messageName += std::to_string(i);
-                                MsgSpatial3D::Description &message = model.newMessage<MsgSpatial3D>(messageName);
-                                // Set the range and bounds.
-                                message.setRadius(env.getProperty<float>("INTERACTION_RADIUS"));
-                                message.setMin(env.getProperty<float>("MIN_POSITION"), env.getProperty<float>("MIN_POSITION"), env.getProperty<float>("MIN_POSITION"));
-                                message.setMax(env.getProperty<float>("MAX_POSITION"), env.getProperty<float>("MAX_POSITION"), env.getProperty<float>("MAX_POSITION"));
-                                // A message to hold the location of an agent.
-                                message.newVariable<int>("id");
-                                // X Y Z are implicit.
-                                // message.newVariable<float>("x");
-                                // message.newVariable<float>("y");
-                                // message.newVariable<float>("z");
-                                message.newVariable<float>("fx");
-                                message.newVariable<float>("fy");
-                                message.newVariable<float>("fz");
+                                if (experiment.spatial) {
+                                    MsgSpatial3D::Description &message = model.newMessage<MsgSpatial3D>(messageName);
+                                    // Set the range and bounds.
+                                    message.setRadius(env.getProperty<float>("INTERACTION_RADIUS"));
+                                    message.setMin(env.getProperty<float>("MIN_POSITION"), env.getProperty<float>("MIN_POSITION"), env.getProperty<float>("MIN_POSITION"));
+                                    message.setMax(env.getProperty<float>("MAX_POSITION"), env.getProperty<float>("MAX_POSITION"), env.getProperty<float>("MAX_POSITION"));
+                                    // A message to hold the location of an agent.
+                                    message.newVariable<int>("id");
+                                    // X Y Z are implicit.
+                                    // message.newVariable<float>("x");
+                                    // message.newVariable<float>("y");
+                                    // message.newVariable<float>("z");
+                                    message.newVariable<float>("fx");
+                                    message.newVariable<float>("fy");
+                                    message.newVariable<float>("fz");
+                                } else {
+                                    MsgBruteForce::Description &message = model.newMessage<MsgBruteForce>(messageName);
+                                    // A message to hold the location of an agent.
+                                    message.newVariable<int>("id");
+                                    message.newVariable<float>("x");
+                                    message.newVariable<float>("y");
+                                    message.newVariable<float>("z");
+                                    message.newVariable<float>("fx");
+                                    message.newVariable<float>("fy");
+                                    message.newVariable<float>("fz");
+                                }
+                                
                             }
                         }
                         for (unsigned int i = 0; i < populationSizes.size(); i++) {
@@ -478,8 +710,19 @@ int main(int argc, const char ** argv) {
                                 outputFuncName += std::to_string(i);
                                 std::string inputFuncName = "inputdata";
                                 inputFuncName += std::to_string(i);
-                                agent.newRTCFunction(agentName + outputFuncName, outputdata).setMessageOutput(messageName);
-                                agent.newRTCFunction(agentName + inputFuncName, inputdata).setMessageInput(messageName);
+                                
+                                // Use brute force or spatial?
+                                const char* outputFuncSource;
+                                const char* inputFuncSource;
+                                if (experiment.spatial) {
+                                    outputFuncSource = outputdata;
+                                    inputFuncSource = inputdata;
+                                } else {
+                                    outputFuncSource = outputdataBruteForce;
+                                    inputFuncSource = inputdataBruteForce;
+                                }
+                                agent.newRTCFunction(agentName + outputFuncName, outputFuncSource).setMessageOutput(messageName);
+                                agent.newRTCFunction(agentName + inputFuncName, inputFuncSource).setMessageInput(messageName);
                             }
                         }
 
